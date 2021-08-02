f280589c2b81e85ef6f436a270b27c243c4e1bfa
Rohit Patil <superfissionr45@gmail.com>
2021-08-02 20:22:41 +0530

    markdown code complete

--- markdown/src/App.js
+++ markdown/src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ReactMarkdown from "react-markdown";
+import "./App.css";
 
 function App() {
+  const [markdown, SetMarkdown] = React.useState(
+    "## Markdown Page - Check Markdown online and type here"
+  );
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <div>
+        <textarea
+          value={markdown}
+          onChange={(e) => {
+            SetMarkdown(e.target.value);
+          }}
+          className="mark"
+        />
+      </div>
+      <article>
+        <ReactMarkdown className="mark">{markdown}</ReactMarkdown>
+      </article>
+    </main>
   );
 }
 
